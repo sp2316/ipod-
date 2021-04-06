4648efd5cf7a81785895e4b3fa4a6cbf1cbc78cb
Sai prasad <sp@MacBook-Pro.local>
2021-04-06 17:43:17 +0530

    Add Zingtouch,a library that helps to handle rotation events and then use it on the outercontroller to capture rotations

--- src/ControlPad.js
+++ src/ControlPad.js
@@ -1,21 +1,35 @@
 import React from 'react'
 import './controlpad.css'
+import ZingTouch from 'zingtouch'
 class ControlPad extends React.Component{
 
-  
+
+    captureRotation=()=>{
+
+        var angle=0;
+        const target=document.getElementById('outer-wheel');
+        const region=new  ZingTouch.Region(target);
+        region.bind(target,'rotate',(e)=>{
+            // angle+=e.detail.distanceFromLast;
+            console.log("wheel");
+        })
+
+    }
     render(){
         return(
 
-                <div className="outerController">
+                <div className="outerController" id="outer-wheel" onClick={this.captureRotation}>
+                   
                     <p className="menu">MENU</p>
+                   
                     <p className="prev">|&lt;&lt;</p>
+                   
                     <p className="forward">&gt;&gt;|</p>
+                   
                     <p className="play-pause">&gt;||</p>
+                   
                       <div className="innerController">
-                        
-
                       </div>
-
  
 
                 </div>
