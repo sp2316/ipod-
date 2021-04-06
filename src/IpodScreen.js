d0c89e91f7f7054adcfe55593b1408ff52f17175
Sai prasad <sp@MacBook-Pro.local>
2021-04-06 18:09:00 +0530

    Add screen component and side menu

--- src/IpodScreen.js
+++ src/IpodScreen.js
@@ -10,7 +10,14 @@ class IpodScreen extends React.Component{
        
         return(
             <div className="ipod-screen" > 
+                <ul id="list">
 
+                <li className="selected">Songs</li>
+                <li id="Album" >Album</li>
+                <li id="Artist">Artist</li>
+                <li id="Settings">Setting</li>
+
+                </ul>
             </div>
         )
     }
