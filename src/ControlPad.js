e59958d7f198eb2845f3a1d811cdc1303fee76db
Sai prasad <sp@MacBook-Pro.local>
2021-04-06 17:31:40 +0530

    Design control pad component

--- src/ControlPad.js
+++ src/ControlPad.js
@@ -1,15 +1,25 @@
 import React from 'react'
+import './controlpad.css'
 class ControlPad extends React.Component{
 
   
     render(){
         return(
-            <div className="control-pad">
-       
 
-            
-                
-            </div>
+                <div className="outerController">
+                    <p className="menu">MENU</p>
+                    <p className="prev">|&lt;&lt;</p>
+                    <p className="forward">&gt;&gt;|</p>
+                    <p className="play-pause">&gt;||</p>
+                      <div className="innerController">
+                        
+
+                      </div>
+
+ 
+
+                </div>
+
         )
     }
 
